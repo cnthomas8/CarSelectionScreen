46d41eff9af622d6488db8ec25fb4bbc8e25013b
Thomas <Thomas@Thomass-iMac.local>
2016-02-04 12:05:19 -0500

    Initial commit

---
+++ app/src/main/java/com/example/thomas/carselectionscreen/SecondScreen.java
@@ -0,0 +1,18 @@
+package com.example.thomas.carselectionscreen;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Thomas on 1/27/16.
+ */
+public class SecondScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_screen);
+
+
+    }
+}
